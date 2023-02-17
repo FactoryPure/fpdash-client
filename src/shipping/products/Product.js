febd0f3f2b969f796459215943593bae56a5df9a
geofffactorypure <gjarman@factorypure.com>
2023-02-17 08:07:37 -0600

    adding conditional to product screen for some reason

--- src/shipping/products/Product.js
+++ src/shipping/products/Product.js
@@ -3,17 +3,17 @@ export default function Product({ product, setFocus }) {
         <div onClick={() => setFocus(product)} className="products__product">
             <p className="products__product__title">{product.title}</p>
             <div className="products__product__active">
-                <p className="products__product__active__main-heading">{product.activeMessage.message_id}</p>
+                <p className="products__product__active__main-heading">{product.activeMessage && product.activeMessage.message_id}</p>
                 <div class="products__product__active__row">
                     <div className="products__product__active__item">
                         <p class="products__product__active__heading">PDP</p>
-                        <p>{product.activeMessage.pdp_line_1}</p>
-                        <p>{product.activeMessage.pdp_line_2}</p>
+                        <p>{product.activeMessage && product.activeMessage.pdp_line_1}</p>
+                        <p>{product.activeMessage && product.activeMessage.pdp_line_2}</p>
                     </div>
                     <div className="products__product__active__item">
                         <p class="products__product__active__heading">CART</p>
-                        <p>{product.activeMessage.cart_line_1}</p>
-                        <p>{product.activeMessage.cart_line_2}</p>
+                        <p>{product.activeMessage && product.activeMessage.cart_line_1}</p>
+                        <p>{product.activeMessage && product.activeMessage.cart_line_2}</p>
                     </div>
                 </div>
             </div>
